306a7c097fff345a3245c92527b10c4b85af8380
Himanshu Chauhan <hchauhan@ventanamicro.com>
2025-02-27 18:06:24 +0530

    riscv: Add functions to register ghes having SSE notification
    Add functions to register the ghes entries which have SSE as
    notification type. The vector inside the ghes is the SSE event
    ID that should be registered.
    
    Signed-off-by: Himanshu Chauhan <hchauhan@ventanamicro.com>
    Link: https://lore.kernel.org/r/20250227123628.2931490-7-hchauhan@ventanamicro.com
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- include/linux/riscv_sse.h
+++ include/linux/riscv_sse.h
@@ -12,6 +12,8 @@
 struct sse_event;
 struct pt_regs;
 
+struct ghes;
+
 typedef int (sse_event_handler)(u32 event_num, void *arg, struct pt_regs *regs);
 
 #ifdef CONFIG_RISCV_SSE
@@ -23,13 +25,16 @@ void sse_event_unregister(struct sse_event *evt);
 
 int sse_event_set_target_cpu(struct sse_event *sse_evt, unsigned int cpu);
 
+int sse_register_ghes(struct ghes *ghes, sse_event_handler *lo_cb,
+		      sse_event_handler *hi_cb);
+int sse_unregister_ghes(struct ghes *ghes);
+
 int sse_event_enable(struct sse_event *sse_evt);
 
 void sse_event_disable(struct sse_event *sse_evt);
 
 int sse_event_enable_local(struct sse_event *sse_evt);
 int sse_event_disable_local(struct sse_event *sse_evt);
-
 #else
 static inline struct sse_event *sse_event_register(u32 event_num, u32 priority,
 						   sse_event_handler *handler,
@@ -42,6 +47,17 @@ static inline void sse_event_unregister(struct sse_event *evt) {}
 
 static inline int sse_event_set_target_cpu(struct sse_event *sse_evt,
 					   unsigned int cpu)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int sse_register_ghes(struct ghes *ghes, sse_event_handler *lo_cb,
+				    sse_event_handler *hi_cb)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int sse_unregister_ghes(struct ghes *ghes)
 {
 	return -EOPNOTSUPP;
 }
